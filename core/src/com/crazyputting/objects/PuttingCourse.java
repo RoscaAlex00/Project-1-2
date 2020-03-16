3d4e086cafc8d7d3f232a8f4e6117f440445621a
Stefan314 <stefan.1999.deelen@gmail.com>
2020-03-16 13:38:10 +0100

    Updated framework

--- core/src/com/crazyputting/objects/PuttingCourse.java
+++ core/src/com/crazyputting/objects/PuttingCourse.java
@@ -4,25 +4,31 @@ import com.badlogic.gdx.math.Vector3;
 import com.crazyputting.function.Function;
 
 public class PuttingCourse {
+
+    private Function height;
+    private Vector3 flag;
+    private Vector3 start;
+    private double frictionCoefficient;
+    final private double maximumVelocity = 3.0; // in meters per second
+
     public PuttingCourse(Function height,
-                         Vector3 flag, Vector3 start){
-
+                         Vector3 flag, Vector3 start, double mu){
+        this.height = height;
+        this.flag = flag;
+        this.start = start;
+        frictionCoefficient = mu;
     }
-    public void get_height(){
 
-    }
-    public void get_flag_position(){
+    public double get_height(Vector3 pos){ return height.evaluate(pos); }
 
-    }
-    public void get_start_position(){
+    public Vector3 get_flag_position(){ return flag; }
 
-    }
-    public void get_friction_coefficient(){
+    public Vector3 get_start_position(){ return start; }
 
-    }
-    public void get_maximum_velocity(){
+    public double get_friction_coefficient(){ return frictionCoefficient; }
 
-    }
+    public double get_maximum_velocity(){ return maximumVelocity; }
+
     public void get_hole_tolerance(){
 
     }
