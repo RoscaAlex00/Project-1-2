f56f1734d5fae0b5e69f7a09ac9d4bba87de41ca
Frunz <f.gharagyozyan@student.maastrichtuniversity.nl>
2020-03-16 14:06:59 +0100

    Updated framework

--- core/src/com/crazyputting/objects/PuttingCourse.java
+++ core/src/com/crazyputting/objects/PuttingCourse.java
@@ -3,8 +3,11 @@ package com.crazyputting.objects;
 import com.badlogic.gdx.math.Vector3;
 import com.crazyputting.function.Function;
 
+import java.util.ArrayList;
+
 public class PuttingCourse {
 
+    private String name;
     private Function height;
     private Vector3 flag;
     private Vector3 start;
@@ -12,24 +15,37 @@ public class PuttingCourse {
     final private double maximumVelocity = 3.0; // in meters per second
 
     public PuttingCourse(Function height,
-                         Vector3 flag, Vector3 start, double mu){
+                         Vector3 flag, Vector3 start, double mu) {
         this.height = height;
         this.flag = flag;
         this.start = start;
         frictionCoefficient = mu;
     }
 
-    public double get_height(Vector3 pos){ return height.evaluate(pos); }
+    public double get_height(Vector3 pos) {
+        return height.evaluate(pos);
+    }
 
-    public Vector3 get_flag_position(){ return flag; }
+    public Vector3 get_flag_position() {
+        return flag;
+    }
 
-    public Vector3 get_start_position(){ return start; }
+    public Vector3 get_start_position() {
+        return start;
+    }
 
-    public double get_friction_coefficient(){ return frictionCoefficient; }
+    public double get_friction_coefficient() {
+        return frictionCoefficient;
+    }
 
-    public double get_maximum_velocity(){ return maximumVelocity; }
+    public double get_maximum_velocity() {
+        return maximumVelocity;
+    }
 
-    public void get_hole_tolerance(){
+    public void get_hole_tolerance() {
+    }
 
+    public String get_Name() {
+    return name;
     }
 }
