325f7764a3b5a3507363b2eab7662e164ac5cc60
GuillaumeThirion <56159753+GuillaumeThirion@users.noreply.github.com>
2020-03-17 18:25:24 +0100

    Update Function.java

--- core/src/com/crazyputting/function/Function.java
+++ core/src/com/crazyputting/function/Function.java
@@ -7,4 +7,5 @@ public interface Function {
         float calcXDeriv(float x,float y);
         float calcYDeriv(float x,float y);
         double evaluate(Vector3 pos);
+        float evaluateF(float f, float g);
 }
