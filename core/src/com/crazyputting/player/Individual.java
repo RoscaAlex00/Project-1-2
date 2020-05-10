ab382dab4dc4bc96f4d0bf4f9efec19a59a2d455
RoscaAlex00 <56193131+RoscaAlex00@users.noreply.github.com>
2020-05-10 16:44:03 +0300

    fix compile error

---
+++ core/src/com/crazyputting/player/Individual.java
@@ -0,0 +1,44 @@
+package com.crazyputting.player;
+
+import java.util.Random;
+
+import com.badlogic.gdx.math.Vector3;
+import com.crazyputting.objects.Ball;
+import com.crazyputting.objects.Hole;
+import com.crazyputting.objects.Terrain;
+
+public class Individual {
+
+	private Terrain terrain;
+	private Hole hole;
+    private Ball ball;    
+
+    private float fitness;
+    private Vector3 shotVelocity;
+    
+    public Individual(Terrain terrain) {
+        Random rn = new Random();
+        
+        this.terrain = terrain;
+        this.hole = terrain.getHole();
+        this.ball = terrain.getBall();
+        shotVelocity = new Vector3(rn.nextFloat(), rn.nextFloat(), rn.nextFloat()); // Needs to be changed (only reflects the idea)
+        ball.hit(shotVelocity);
+
+        fitness = Float.MAX_VALUE;
+    }
+
+    //Calculate fitness
+    public void calcFitness() {
+    	Vector3 holePos = hole.getPosition().cpy();
+        Vector3 ballPos = ball.getPosition().cpy();
+        Vector3 ballToHoleDistance = holePos.sub(ballPos);
+        ballToHoleDistance.z = 0;
+        fitness = ballToHoleDistance.len();
+    }
+
+	public float getFitness() {
+		calcFitness();
+		return fitness;
+	}
+}
